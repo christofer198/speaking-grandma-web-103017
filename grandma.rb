02f900e5c4672943ba712122e17f84b851d57aea
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-12 16:09:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(speech)
 # unless you shout it (type in all capitals).
 
   if speech == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif speech == speech.upcase
     puts "NO, NOT SINCE 1938!"
   else
